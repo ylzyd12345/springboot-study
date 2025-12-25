ad8f264cb85c944e250b3490ba103419f998fc45
Gaofeng.Qi <290378941@qq.com>
2025-12-25 17:54:00 +0800

    run

--- spring4demo/spring4demo-starter/src/main/java/com/kev1n/spring4demo/starter/Spring4demoApplication.java
+++ spring4demo/spring4demo-starter/src/main/java/com/kev1n/spring4demo/starter/Spring4demoApplication.java
@@ -1,17 +1,17 @@
-package com.kev1n.spring4demo.web;
+package com.kev1n.spring4demo.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Spring4demo Web应用启动类
+ * Spring4demo 应用启动类
  * 
  * @author spring4demo
  * @version 1.0.0
  */
 @SpringBootApplication(scanBasePackages = "com.kev1n.spring4demo")
-public class Spring4demoWebApplication {
+public class Spring4demoApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Spring4demoWebApplication.class, args);
+        SpringApplication.run(Spring4demoApplication.class, args);
     }
 }
